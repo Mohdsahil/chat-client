527cb3a0c13a96f35ee7db7b8c087b1ece263f98
rblockchaindeveloper <rblockchaindeveloper@gmail.com>
2024-09-27 12:38:58 +0530

    fix env

--- src/configs/envVariables.js
+++ src/configs/envVariables.js
@@ -2,7 +2,7 @@ const Reset = '\x1b[0m';
 const FgYellow = '\x1b[33m';
 
 const APP_ENV = 'local'
-let APP_HOST = REACT_APP_APP_HOST
+let APP_HOST = process.env.REACT_APP_APP_HOST
 
 
 console.log(FgYellow, APP_ENV, Reset)
